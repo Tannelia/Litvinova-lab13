d0e92b9e5a30a9713d87b790d33d894f21b9149d
Pavel Myasoedov <meatfay@gmail.com>
2016-07-03 22:13:30 +0300

    Some fixes

--- src/ru/spb/herzen/second/CounterSynchronized.java
+++ src/ru/spb/herzen/second/CounterSynchronized.java
@@ -1,8 +1,5 @@
 package ru.spb.herzen.second;
 
-/**
- * Created by pawheel on 30.06.16.
- */
 public class CounterSynchronized extends Thread {
     synchronized public void run(){
         for(Integer i=1; i<=100; i++){
